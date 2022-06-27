0f06b41a2433bfff123192e8afc7a4affa38d076
shreya deo <shreyadeo1998@gmail.com>
2022-06-27 18:40:02 +0530

    CI CD

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting ML project"
+    return "CI CD Pipeline established!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
